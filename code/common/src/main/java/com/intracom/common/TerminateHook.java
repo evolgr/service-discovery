a1f0698313ec85ca15f9886f06e9689b3fc1d6f2
zpalele <zpalele@ericsson.com>
2022-11-14 16:22:21 +0100

    Implementation of TerminateHook

--- code/common/src/main/java/com/intracom/common/TerminateHook.java
+++ code/common/src/main/java/com/intracom/common/TerminateHook.java
@@ -2,22 +2,47 @@ package com.intracom.common;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Completable;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class TerminateHook
 {
     private static final Logger log = LoggerFactory.getLogger(TerminateHook.class);
+    private final Completable vmShutdown;
+    private final CompletableFuture<Object> shutdownComplete = new CompletableFuture<>();
 
     public TerminateHook()
     {
-		// empty constructor
-	}
+        this.vmShutdown = Completable.create(emitter ->
+        {
+            final var hook = new Thread(() ->
+            {
+                log.info("JVM is shutting down");
+                emitter.onComplete();
 
-	public static void main(String args[]) throws InterruptedException {
-		while(true)
-		{
-			log.info("this is a simple example");
-			TimeUnit.SECONDS.sleep(10);
-		}
+                try
+                {
+                    shutdownComplete.get();
+                }
+                catch (InterruptedException e)
+                {
+                    Thread.currentThread().interrupt();
+                }
+                catch (ExecutionException e)
+                {
+                    // Ignore exception, continue JVM shutdown
+                }
+            });
+            hook.setName(TerminateHook.class.getName());
+            Runtime.getRuntime().addShutdownHook(hook);
+        }).cache();
 	}
+    
+    public Completable get()
+    {
+        return this.vmShutdown;
+    }
 }
