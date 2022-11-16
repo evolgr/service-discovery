3626e3f210cd497c0c7135a885f0404246616bde
Evaggelos Kottis <evaggelos.kottis@ericsson.com>
2022-11-16 22:11:58 +0200

    json schema
    - service discovery/registry schema
    - update in .gitignore
    - remove eclipse automatic copyright statement

--- code/common-lib/src/main/java/com/intracom/common/web/WebClientCBuilder.java
+++ code/common-lib/src/main/java/com/intracom/common/web/WebClientCBuilder.java
@@ -1,18 +1,3 @@
-/**
- * COPYRIGHT ERICSSON GMBH 2022
- *
- * The copyright to the computer program(s) herein is the property
- * of Ericsson GmbH, Germany.
- *
- * The program(s) may be used and/or copied only with the written
- * permission of Ericsson GmbH in accordance with
- * the terms and conditions stipulated in the agreement/contract
- * under which the program(s) have been supplied.
- *
- * Created on: Nov 14, 2022
- *     Author: zpalele
- */
-
 package com.intracom.common.web;
 
 import io.reactivex.functions.Consumer;
